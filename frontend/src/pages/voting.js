ccb8d32b96797fe5940f4e47f97ecb3268676db7
tom-mate-o <132936694+tom-mate-o@users.noreply.github.com>
2024-04-24 15:28:24 +0200

    update

--- frontend/src/pages/voting.js
+++ frontend/src/pages/voting.js
@@ -123,16 +123,16 @@ export default function Voting() {
                 <i>
                   <p className="song">"{country.song}"</p>
                 </i>
-                {/* <iframe
+                <iframe
                   style={{ borderRadius: '12px' }}
-                  src="https://open.spotify.com/embed/track/0uHrMbMv3c78398pIANDqR?utm_source=generator&theme=0"
+                  src={country.spotify}
                   width="100%"
                   height="152"
                   frameBorder="0"
                   allowFullScreen=""
                   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                   loading="lazy"
-                ></iframe> */}
+                ></iframe>
                 <hr />
               </div>
             </VoteContainer>
